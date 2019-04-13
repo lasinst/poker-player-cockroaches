fd97d1b9183ae21ce3505719512c761d3ac77e58
Bence Zilaji <bzilaji@instructure.com>
2019-04-13 12:13:22 -0600

    fixed errors, and doing all in strategy

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -34,7 +34,7 @@ public class Player {
     private static int getRandomNumberInRange(int min, int max) {
 
         if (min >= max) {
-            throw new IllegalArgumentException("max must be greater than min");
+            return max;
         }
 
         Random r = new Random();
