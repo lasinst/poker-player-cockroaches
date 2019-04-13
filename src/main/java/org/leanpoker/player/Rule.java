977c1a9d44150a36d01e8138cbfae134572501af
Bence Zilaji <bzilaji@instructure.com>
2019-04-13 14:15:51 -0600

    added few figure detection

--- src/main/java/org/leanpoker/player/Rule.java
+++ src/main/java/org/leanpoker/player/Rule.java
@@ -10,11 +10,31 @@ public class Rule {
         if (hasPoker(cards)) {
             return Hands.POKER;
         }
-        
+        if (hasFullHouse(cards)) {
+            return Hands.FULL_HOUSE;
+        }
+
+        if (hasDrill(cards)) {
+            return Hands.DRILL;
+        }
+
+        if (hasTwoPair(cards)) {
+            return Hands.TWO_PAIR;
+        }
+
         if (hasPair(cards)) {
             return Hands.PAIR;
         }
+
         return Hands.NOTHING;
+    }
+
+    private static boolean hasFullHouse(List<Card> cards) {
+        return hasPair(cards) && hasDrill(cards);
+    }
+
+    private static boolean hasDrill(List<Card> cards) {
+        return areRankSame(cards, 3);
     }
 
     private static boolean hasPair(List<Card> cards) {
@@ -23,6 +43,10 @@ public class Rule {
 
     private static boolean hasPoker(List<Card> cards) {
         return areRankSame(cards, 4);
+    }
+
+    private static boolean hasTwoPair(List<Card> cards) {
+        return areTwoSame(cards);
     }
 
     private static boolean areRankSame(List<Card> cards, int number) {
@@ -44,5 +68,27 @@ public class Rule {
             }
         }
         return false;
+    }
+
+    private static boolean areTwoSame(List<Card> cards) {
+        int pairs = 0;
+        if (cards.size() >= 4) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for (Card card : cards) {
+                Integer integer = map.get(card.rank.getValue());
+                if (integer == null) {
+                    integer = 1;
+                } else {
+                    integer++;
+                }
+                map.put(card.rank.getValue(), integer);
+            }
+            for (int i : map.values()) {
+                if (i == 2) {
+                    pairs++;
+                }
+            }
+        }
+        return pairs == 2;
     }
 }
