cc1c7f02916394906883b9eed81ea5ece705c1bb
Bence Zilaji <bzilaji@instructure.com>
2019-04-13 14:42:49 -0600

    updated rules.

--- src/main/java/org/leanpoker/player/Rule.java
+++ src/main/java/org/leanpoker/player/Rule.java
@@ -1,17 +1,30 @@
 package org.leanpoker.player;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Rule {
 
     static Hands getHands(List<Card> cards) {
+
+        if (hasStaightFlush(cards)) {
+            return Hands.STRAIGHT_FLUSH;
+        }
+
         if (hasPoker(cards)) {
             return Hands.POKER;
         }
+
         if (hasFullHouse(cards)) {
             return Hands.FULL_HOUSE;
+        }
+
+        if (hasStraight(cards)) {
+            return Hands.STRAIGHT;
+        }
+
+        if (hasFlush(cards)) {
+            return Hands.FLUSH;
         }
 
         if (hasDrill(cards)) {
@@ -27,6 +40,52 @@ public class Rule {
         }
 
         return Hands.NOTHING;
+    }
+
+    private static boolean hasStaightFlush(List<Card> cards) {
+        return hasStraight(cards) && hasFlush(cards);
+    }
+
+    private static boolean hasStraight(List<Card> cards) {
+
+        int consecutive = 0;
+        if (cards.size() >= 5) {
+            Set<Integer> integers = new HashSet<>(7);
+            for (Card card : cards) {
+                integers.add(card.rank.getValue());
+            }
+            List<Integer> sortedList = integers.stream().sorted().collect(Collectors.toList());
+
+            for (int i = 0; i < sortedList.size() - 1; i++) {
+                if (sortedList.get(i) + 1 == sortedList.get(i + 1)) {
+                    consecutive++;
+                } else {
+                    consecutive = 0;
+                }
+            }
+        }
+        return consecutive == 4;
+    }
+
+    private static boolean hasFlush(List<Card> cards) {
+        if (cards.size() >= 5) {
+            Map<Suits, Integer> map = new HashMap<>();
+            for (Card card : cards) {
+                Integer integer = map.get(card.suit);
+                if (integer == null) {
+                    integer = 1;
+                } else {
+                    integer++;
+                }
+                map.put(card.suit, integer);
+            }
+            for (int i : map.values()) {
+                if (i == 5) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     private static boolean hasFullHouse(List<Card> cards) {
